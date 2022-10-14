44c04756e75f033fbecc2e6fe0fa5b931a5cce7e
Paul Morgan <orbitingmind@gmail.com>
2022-10-13 21:12:11 -0400

    this is Gauss sum

---
+++ gaussianSum.js
@@ -0,0 +1,13 @@
+const sum = function(ary){
+
+    const lastNumber = ary[.arylength-1]   
+
+    return (lastNumber * lastNumber +1)/2 
+
+}   
+
+
+let nums = [1,2,3,4]; 
+
+
+console.log(sum(nums));
