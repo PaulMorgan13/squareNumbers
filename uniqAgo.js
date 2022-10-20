c0f1696ac0f7869203fa52f3727f8dd0fe063f70
Paul Morgan <orbitingmind@gmail.com>
2022-10-20 15:06:18 -0400

    this is a unique algo that sort numbers without duplicatesconst uniqSort = function(arry) {

---
+++ uniqAgo.js
@@ -0,0 +1,20 @@
+const uniqSort = function(arry) {
+    const breadcrumbs = {}; // cahce data
+    const result = [arry[0]];
+  
+    for (let i = 0; i < arry.length; i++) {
+      // this loops through every element in the array   
+      if (!breadcrumbs[arry[i]]) {
+        result.push(arry[i]);
+        breadcrumbs[arry[i] = true];
+      }
+    }
+  
+    return result.sort((a, b) => a - b);
+  
+  }
+  
+  
+  const mynums = [6, 2, 3, 3, 5]
+  
+  uniqSort(mynums)  
