22f55654e9bff1477ca686682c935d02fe541424
Paul Morgan <orbitingmind@gmail.com>
2022-10-23 13:06:39 -0400

    added a better solution for the  problem

--- ReturnTheDay.js
+++ ReturnTheDay.js
@@ -28,4 +28,30 @@ function whatday(num) {
     }
   
   
-  }
+  }  
+  
+  // Better solution 
+
+
+function whatday(num) {
+
+    let days = {
+        1:"Sunday",
+        2:"Monday",
+        3:"Tuesday",
+        4:"Wednesday", 
+        5:"Thursday",
+        6:"Friday",
+        7:"Saturday",
+    }
+    return day[num] || "Wrong, please enter a number between 1 and 7"
+
+}
+
+
+
+
+
+
+
+
