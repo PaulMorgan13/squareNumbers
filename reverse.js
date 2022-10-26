bf00d02d55752daa22540e3190a0c21be0f60a12
Paul Morgan <orbitingmind@gmail.com>
2022-10-25 20:48:18 -0400

    minor save

---
+++ reverse.js
@@ -0,0 +1,15 @@
+
+reverse = function(array) {
+
+    let  newList = [];
+    
+      for(i = array.length-1; i >= 0; i--){
+      
+        newList.push(array[i]);
+    
+        
+        
+      }
+      
+     return newList;
+    
