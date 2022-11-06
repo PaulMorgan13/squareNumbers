1490c41b95b406d9c6fbdc2a45f5768ffc1e87ee
Paul Morgan <orbitingmind@gmail.com>
2022-11-05 22:29:11 -0400

    added a shorter solution

--- isPalindrome.js
+++ isPalindrome.js
@@ -16,4 +16,11 @@ function isPalindrome(x) {
     }
   
   
+  }  
+
+
+  function isPalindrome(x){
+      let newWord = x.split("").reverse().join("").toLowerCase()
+
+      return x==newWord? true:false;
   }
