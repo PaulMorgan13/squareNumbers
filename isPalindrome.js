132a4ec47c330585b307813a2094ff2b3f3f45f1
Paul Morgan <orbitingmind@gmail.com>
2022-11-12 01:00:12 -0500

    removed longer solution

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,22 +1,3 @@
-function isPalindrome(x) {  
-  
-    let str = x.toLowerCase();
-      
-    let newString = ""; 
-    
-    for (let i = str.length-1; i >= 0 ; i--){
-      newString = newString + str[i];
-    }  
-    
-    if(str == newString){
-      return true
-    }
-    else {
-      return false
-    }
-  
-  
-  }  
 
 
   function isPalindrome(x){
