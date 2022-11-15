70b2de97c9f84eee17fa8df8d7c976304a328c89
Paul Morgan <orbitingmind@gmail.com>
2022-11-14 22:46:19 -0500

    this includes the shorter solution

--- openOrsenior.js
+++ openOrsenior.js
@@ -1,10 +1,9 @@
-
-
 function openOrSenior(data){
 
+    
+    return newArray = data.map((x )=> { 
 
-    return newArray = data.map((x )=> {
-      if (x[0] < 55  &&  x[1] < 7 ){
+      if (x[0] < 55  &&  x[1] < 7 ){ 
         return  "Open"
       }    
       else 
@@ -14,4 +13,12 @@ function openOrSenior(data){
    
    console.log(newArray)
    
-   }
+   }   
+
+
+   //the less code solution 
+
+   function openOrSenior(data){
+    return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+  }
+  
