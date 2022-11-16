75f3c074e55b735dc6b7ec7020f1ec473f8d1978
Paul Morgan <orbitingmind@gmail.com>
2022-11-15 20:54:50 -0500

    first commit

--- openOrsenior.js
+++ openOrsenior.js
@@ -18,6 +18,7 @@ function openOrSenior(data){
 
    //the less code solution 
 
+   
    function openOrSenior(data){
     return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
   }
