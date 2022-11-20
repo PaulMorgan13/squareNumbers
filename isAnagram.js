79a42417f45d6025132e69a81fb0168d90e8330f
Paul Morgan <orbitingmind@gmail.com>
2022-11-20 18:55:30 -0500

    added coments to that code

--- isAnagram.js
+++ isAnagram.js
@@ -1,9 +1,13 @@
  
 var isAnagram = (test, og) =>{  
+  
 
+    // takes  input and then converts to lowercaes. then break that string into an array. then sort that arry and combine
     let t = test.toLowercase().split("").sort().join(""); 
     let o = og.toLowercase().spit("").sort().join(""); 
     
+
+    //checks if that og is the same word as  test input and returns a boolen 
     if (t==o){
         return true 
 
