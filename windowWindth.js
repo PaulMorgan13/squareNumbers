5b69b15ebcdc92691cb27d2a57a44f0982ca3dee
Paul Morgan <orbitingmind@gmail.com>
2022-11-27 23:44:45 -0500

    add the small comment

--- windowWindth.js
+++ windowWindth.js
@@ -8,10 +8,12 @@ function changeColor() {
     //grabs window width
     window_width = window.innerWidth;
 
+
     if(window_width > 800){
         back.style.background = "red"
     } 
-    else{
+    else{ 
+        //changes the color to plum if width is above 800
         back.style.background = "plum"
 
     }
