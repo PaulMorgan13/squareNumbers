aed45c325a6c0a1adb621ebaa395a1f4254afb12
Paul Morgan <orbitingmind@gmail.com>
2022-11-29 22:12:11 -0500

    added class to file

--- hashMap.js
+++ hashMap.js
@@ -15,7 +15,24 @@ console.log(newMap);
 newMap.delete("apple")   
 
 
-for (x of newMap){
-  console.log("this object  " + x[1])
-}
+class Contact{
+  constructor(name, age , number){
+    this.name = name,
+    this.age =age,
+    this.number = number;
+  }
+
+}  
+
+
+const contacts = new Map(); 
+
+contacts.set("paul",new Contact("paul",29,"777888"));
+contacts.set("tim",new Contact("tim",49,"777888"));
+contacts.set("juntao",new Contact("juntao",78,"777888"));     
+console.log(contacts)
+
+contacts.forEach((x) => {
+  console.log("hi " + x.name)
+})
 
