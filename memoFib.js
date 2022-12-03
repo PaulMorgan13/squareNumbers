76666d59eaec72f658b61483a3c125f2d2de7b05
Paul Morgan <orbitingmind@gmail.com>
2022-12-02 22:44:59 -0500

    added comment

---
+++ memoFib.js
@@ -0,0 +1,24 @@
+function fib(n, prev = []){  
+
+  // check if number si in array
+    if(prev[n] != null){
+      return prev[n]
+    }
+    
+    let result 
+    if (n < 2){
+      result = 1
+    }
+    else {
+      result = fib(n-1, prev = [])+ fib(n-2, prev = []);
+    }
+  
+    prev[n] = result
+    return result
+  
+    
+  }   
+  
+  
+  
+  console.log(fib(40))   
