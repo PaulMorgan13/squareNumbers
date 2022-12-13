ae334dfb19d415869243c2ee47537290b192f72d
Paul Morgan <orbitingmind@gmail.com>
2022-12-12 22:30:25 -0500

    added comments

--- removeMark.js
+++ removeMark.js
@@ -1,8 +1,10 @@
 function removeExclamationMarks(s) {    
     
+
+    //spit method breaks down the array amd remove the element with "!" the join combines the array to a string
     let newWord = s.split("!").join("")
     
     
     return newWord
     
-  }  
+  }           
