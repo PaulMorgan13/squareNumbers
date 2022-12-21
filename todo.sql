949208da0cd32a75eedaeaab234a63e9219a47d2
Paul Morgan <orbitingmind@gmail.com>
2022-12-20 21:28:36 -0500

    second commit

---
+++ todo.sql
@@ -0,0 +1,26 @@
+--create   
+--INSERT into todos (task, task_des) values('Anki','practice syntax');    
+
+--INSERT into todos (task, task_des) values('code ','practice fucntions');  
+
+
+
+--read
+--select task, task_des from todos  where id < 4;   
+
+
+--update      
+
+--update todos set status = false where id = 3;   
+
+--select * from todos order by id desc;  aal
+
+
+--delete  
+
+delete from todos where task = 'code '; 
+
+select * from todos;
+
+
+
