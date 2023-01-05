a44bcb0e5cc622cf0016a4d7242f3cd745d13d33
Paul Morgan <orbitingmind@gmail.com>
2023-01-04 20:28:15 -0500

    uploading new String repeat

---
+++ stringRepeat.js
@@ -0,0 +1,18 @@
+function repeatStr (n, s) {
+  
+
+    //create empty string 
+    let newString = ""; 
+    
+    // loop the array n times
+    for (i = 0; i < n; i++){
+      
+
+    // Sting summation
+     newString = s + newString 
+    }
+    
+    
+    //return the newly created string
+    return newString;
+  }
