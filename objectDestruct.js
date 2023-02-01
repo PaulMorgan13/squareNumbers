18ded89f83e2cfce74b93d8683261fe0f1e1eabd
Paul Morgan <orbitingmind@gmail.com>
2023-01-31 23:56:49 -0500

    object destructuring

---
+++ objectDestruct.js
@@ -0,0 +1,19 @@
+const person = {
+
+    "name":"paul",
+    "age":29, 
+    "isSingle":true,
+
+}   
+
+//regular
+const {name, age , isSingle} = person 
+
+
+const {name:x, age:y , isSingle:z} = person 
+
+
+
+
+
+console.log(`my name is ${x}`)
