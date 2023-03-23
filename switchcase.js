3a5cf878c64a0e99cbccb6c455f8a2e6db603294
Paul Morgan <orbitingmind@gmail.com>
2023-03-22 23:35:57 -0400

    added switch case js

---
+++ switchcase.js
@@ -0,0 +1,19 @@
+let favfood = (fav)=> {
+    switch(fav){
+      case "pie": {
+          console.log("pie is ok")
+      }   
+      case "pears": {
+        console.log(`i like this fruit  bit i think it bland`)
+      } 
+      case "hotdogs":{
+        console.log("this is amazing"
+        )
+      }  
+      default:{
+        console.log("this is mid")
+      }
+    }
+  }   
+  
+  favfood("apple");
